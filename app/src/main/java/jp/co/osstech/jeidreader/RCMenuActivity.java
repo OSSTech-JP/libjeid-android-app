60f88ec4dc36234a3d3d160829fc76f941e68ea1
HAMANO Tsukasa <hamano@osstech.co.jp>
2026-08-13 18:17:00 +0900

    新型在留カード読み出しを有効化

--- app/src/main/java/jp/co/osstech/jeidreader/RCMenuActivity.java
+++ app/src/main/java/jp/co/osstech/jeidreader/RCMenuActivity.java
@@ -7,10 +7,10 @@ public class RCMenuActivity
     protected MenuEntry[] getMenuEntries() {
         return new MenuEntry[] {
             new MenuEntry(R.string.rc1_reader, RCReaderActivity.class),
-            new MenuEntry(R.string.rc2_reader, RC2ReaderActivity.class, true),
+            new MenuEntry(R.string.rc2_reader, RC2ReaderActivity.class),
             // 特定在留カード等は個人番号カード上の在留APだが、在留カード等の
             // データを読み出す機能なのでこのメニューに置く
-            new MenuEntry(R.string.rcs_reader, RCSReaderActivity.class, true),
+            new MenuEntry(R.string.rcs_reader, RCSReaderActivity.class),
         };
     }
 }
